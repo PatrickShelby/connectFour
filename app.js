012939161f43c4b7b6ef7cadbbca4317a7a6bee7
James Newman & Alex Kass <pair+jnewman12.kassanova@devbootcamp.com>
2015-06-18 15:42:56 -0700

    imported files from tic tac toe

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  game = new ConnectFour();
+  game.render('.connect-four');
+
+
+});
+
+
---
+++ connectFour.js
@@ -0,0 +1,19 @@
+/*
+psuedo-code methods
+1.
+
+*/
+
+connectFour = function(){
+      this.board =
+           [null,null,null,null,null,null,null,
+            null,null,null,null,null,null,null,
+            null,null,null,null,null,null,null,
+            null,null,null,null,null,null,null,
+            null,null,null,null,null,null,null,
+            null,null,null,null,null,null,null]
+
+      this.currentPlayer = "red";
+}
+
+
